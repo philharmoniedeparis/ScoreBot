358b467c271a329f094debd4f93e935fafe16fd2
tristandeborde <wWUxFvwN5kEP>
2022-05-04 19:01:52 +0200

    Integrate new sparql query to actions

--- actions/actions.py
+++ actions/actions.py
@@ -26,8 +26,9 @@ class NoEntityFoundException(Exception):
 class ActionGetSheetMusicByCasting(Action):
     def __init__(self):
         super(ActionGetSheetMusicByCasting, self).__init__()
-        self.route_prefix = "PREFIX dc: <http://purl.org/dc/elements/1.1/> PREFIX dct: <http://purl.org/dc/terms/> PREFIX mus: <http://data.doremus.org/ontology#> PREFIX ecrm: <http://erlangen-crm.org/current/> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> select ?score ?scoreUrl ?scoreLabel  ?medium ?mediumLabel where {"
-        self.route_suffix = '?casting mus:U23_has_casting_detail ?castingDetail . ?castingDetail mus:U2_foresees_use_of_medium_of_performance ?medium. ?medium skos:prefLabel ?mediumLabel. filter (lang(?mediumLabel)="fr") ?score mus:U170_has_title_statement ?scoreLabelStatement. ?scoreLabelStatement rdfs:label ?scoreLabel. bind (strafter(str(?score),"ark/49250/")as ?identifier1) BIND(IRI(CONCAT("https://catalogue.philharmoniedeparis.fr/doc/ALOES/", ?identifier)) AS ?scoreUrl)}'
+        self.route_prefix = "PREFIX dc: <http://purl.org/dc/elements/1.1/> PREFIX dct: <http://purl.org/dc/terms/> PREFIX mus: <http://data.doremus.org/ontology#> PREFIX ecrm: <http://erlangen-crm.org/current/> PREFIX skos: <http://www.w3.org/2004/02/skos/core#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX foaf: <http://xmlns.com/foaf/0.1/> select ?score ?scoreUrl  ?scoreLabel ?responsibilityLabel where { "
+        self.route_suffix = "?score mus:U170_has_title_statement ?scoreLabelStatement. ?scoreLabelStatement rdfs:label ?scoreLabel. optional {?score mus:U172_has_statement_of_responsibility_relating_to_title ?U172_has_statement_of_responsibility_relating_to_title.  ?U172_has_statement_of_responsibility_relating_to_title rdfs:label ?responsibilityLabel} bind (strafter(str(?score),\"ark/49250/\")as ?identifier) BIND(IRI(CONCAT(\"https://catalogue.philharmoniedeparis.fr/doc/ALOES/\", ?identifier)) AS ?scoreUrl1) BIND ( substr(str(?scoreUrl1), 1, 58) as ?scoreUrl) } Limit 20"
+        self.allowed_medias = list(medias.iaml.keys()) + list(medias.mimo.keys())
 
     def name(self) -> Text:
         return "action_get_sheet_music_by_casting"
@@ -36,7 +37,6 @@ class ActionGetSheetMusicByCasting(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        channel = tracker.get_latest_input_channel()
         entities = tracker.latest_message['entities']
         logging.info(entities)
         answer =    f"Il me semble que vous voulez obtenir une liste des partitions. "
@@ -46,7 +46,7 @@ class ActionGetSheetMusicByCasting(Action):
                 if ent['entity'] == 'medium':
                     inputted_medias.add(ent.get('value'))
             # if inputted_medias is None, empty, or contains no key of medias.medias, throw error
-            if not inputted_medias or not inputted_medias.issubset(medias.medias.keys()):
+            if not inputted_medias or not inputted_medias.issubset(self.allowed_medias):
                 raise NoEntityFoundException(f"Problem with entities: {inputted_medias}")
             results = self.get_query_results(inputted_medias)
             if not results:
@@ -71,9 +71,14 @@ class ActionGetSheetMusicByCasting(Action):
         return texts
 
     def format_sparql_query(self, inputted_medias: set) -> str:
-        values, query = "", ""
+        values, query = "?score mus:U13_has_casting ?casting . ", ""
         for i, medium in enumerate(inputted_medias):
-            values += f"values (?input_quantity_{i} ?input_medium_{i}) {{ (\"1\"^^xsd:integer <http://data.doremus.org/vocabulary/iaml/mop/{medium}>)}}"
+            # Check if mimo or iaml
+            if medium.isdigit():
+                formatted_medium = f"<http://www.mimo-db.eu/InstrumentsKeywords/{medium}>"
+            else:
+                formatted_medium = f"<http://data.doremus.org/vocabulary/iaml/mop/{medium}>"
+            values += f"values (?input_quantity_{i} ?input_medium_{i}) {{ (\"1\"^^xsd:integer {formatted_medium})}}"
             query += f"?score mus:U13_has_casting ?casting . ?casting mus:U23_has_casting_detail ?castingDetail_{i}. ?castingDetail_{i} mus:U2_foresees_use_of_medium_of_performance ?input_medium_{i} . ?castingDetail_{i} mus:U30_foresees_quantity_of_mop ?input_quantity{i} ."
         parse_query = urllib.parse.quote(self.route_prefix + values + query + self.route_suffix)
         return parse_query
