042c16d328ee1a62c8a796dce403c0a566e0dfc0
Tristan <tr.deborde@gmail.com>
2023-03-04 02:12:40 +0100

    Add new NLU examples following Cecile QA

--- utils/formations_synonyms.py
+++ utils/formations_synonyms.py
@@ -1,11 +1,20 @@
 formations = {
-    "2": ["duos", "duo","duet"],
-    "3": ["trios", "trio"],
-    "4": ["quatuors", "quatuor","quartet","quartette"],
-    "5": ["quintettes", "quintette","quintet","quintuor"],
-    "6": ["sextuors", "sextuor","sextet","sextette"],
-    "7": ["septuors", "septuor","septet","septette"],
-    "8": ["octuors", "octuor","octet","octette"],
-    "9": ["nonettes", "nonette","nonet"],
+    "2": [
+        "duos",
+        "duo",
+        "duet",
+        "duos de",
+    ],
+    "3": [
+        "trios",
+        "trio",
+        "trios de",
+    ],
+    "4": ["quatuors", "quatuor", "quartet", "quartette"],
+    "5": ["quintettes", "quintette", "quintet", "quintuor"],
+    "6": ["sextuors", "sextuor", "sextet", "sextette"],
+    "7": ["septuors", "septuor", "septet", "septette"],
+    "8": ["octuors", "octuor", "octet", "octette"],
+    "9": ["nonettes", "nonette", "nonet"],
     "10": ["dixtuors", "dixtuor"],
 }
